4b4cd3cfbd33814ce7a4e3279d37bfdf6dc6b335
Sergey Fedorov <serge.fdrv@gmail.com>
2023-08-26 12:09:30 +0200

    Create basic website using Docusaurus

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-🚧 UNDER CONSTRUCTION 🚧
+# Replica_IO Website
+
+This is a website for the [Replica_IO][replica-io] project. It is built using
+[Docusaurus 2](https://docusaurus.io/), a modern static website
+generator.
+
+[replica-io]: https://github.com/replica-io/replica-io
+
+### Installation
+
+```sh
+npm install
+```
+
+### Local Development
+
+```sh
+npm run start
+```
+
+This command starts a local development server and opens up a browser
+window. Most changes are reflected live without having to restart the
+server.
+
+### Build
+
+```sh
+npm run build
+```
+
+This command generates static content into the `build` directory and
+can be served using any static contents hosting service.
+
+### Deployment
+
+This website is configured for deployment to [GitHub
+Pages][github-pages]. The deployment is triggered with [GitHub
+Actions][github-actions]; it uses a popular third-party deployment
+action:
+[peaceiris/actions-gh-pages](https://github.com/peaceiris/actions-gh-pages).
+
+[github-pages]: https://pages.github.com/
+[github-actions]: https://help.github.com/actions
+
+### License
+
+The site code is licensed under the [MIT license](LICENSE-code).
+
+The site content in licensed under the [Creative Commons Attribution
+4.0 International](LICENSE-content) license.
---
+++ src/pages/index.js
@@ -0,0 +1,43 @@
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import React from 'react';
+
+import About from './about.md';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero', styles.heroBanner)}>
+      <div className="container">
+        <img src="img/logo.svg" alt="Replica_IO Logo" width="240px" />
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--primary button--lg"
+            to={siteConfig.customFields.gitHubUrl}>
+            Go to GitHub
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      description={siteConfig.customFields.description}>
+      <HomepageHeader />
+      <main>
+        <div className="container container--fluid margin-vert--xl">
+          <About />
+        </div>
+      </main>
+    </Layout>
+  );
+}
