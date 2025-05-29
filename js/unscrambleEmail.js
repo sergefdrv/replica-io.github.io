6064d8e205b0d9f018482574a74597ef78a603d6
sergefdrv <sergefdrv@users.noreply.github.com>
2025-05-29 17:03:57 +0000

    deploy: fa09310a40162500904f1140b38bad895d89bf9b

--- js/unscrambleEmail.js
+++ js/unscrambleEmail.js
@@ -1,1 +1,14 @@
-import ExecutionEnvironment from"@docusaurus/ExecutionEnvironment";import{unscramble}from"botex";const botexKey="CHKXRg4cdMZ0XADF",obfuscatedEmail="BwC1311181fB4C1sBzmC14162k1dByC112z16132t1o1jBh";export function onRouteUpdate({location:o,previousLocation:t}){if(ExecutionEnvironment.canUseDOM&&"/"===o.pathname&&"#contact-email"===o.hash){const o=unscramble(obfuscatedEmail,botexKey);window.location.href=`mailto:${o}`}}
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import { unscramble } from 'botex';
+
+const botexKey = 'CHKXRg4cdMZ0XADF';
+const obfuscatedEmail = 'BwC1311181fB4C1sBzmC14162k1dByC112z16132t1o1jBh';
+
+export function onRouteUpdate({ location, previousLocation }) {
+  if (ExecutionEnvironment.canUseDOM) {
+    if (location.pathname === '/' && location.hash === '#contact-email') {
+      const email = unscramble(obfuscatedEmail, botexKey);
+      window.location.href = `mailto:${email}`;
+    }
+  }
+}
